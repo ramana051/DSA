d63cadaa12190a27b1e5ee4835cb086d910f081a
ramana <veeravenkataramana051@gmail.com>
2025-11-30 05:42:07 +0530

    Day 1: Solved LargestElement.java

---
+++ largestElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class largestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        int largest = findLargestElement(array);
+        System.out.println("The largest element in the array is: " + largest);
+        
+        scanner.close();
+    }
+    
+    public static int findLargestElement(int[] array) {
+        int largest = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+            }
+        }
+        return largest;
+    }
+}
